e09c9aca0234c50d7abffa9a2438706e59571fc1
sampiecz <samuel.piecz@gmail.com>
2020-04-04 13:14:01 -0500

    Created keyword and trend models. Made both objects accessible through the admin interface.

--- gifs/home/admin.py
+++ gifs/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from gifs.home.models import Keyword, Trend
 
 # Register your models here.
+admin.site.register([Trend, Keyword])
--- gifs/home/models.py
+++ gifs/home/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+import datetime
+
+class Trend(models.Model):
+    date = models.DateField(default=datetime.datetime.utcnow)
+    data = models.TextField(blank=True)
+
+    def __str__(self):
+        return str(self.date)
 
 # Create your models here.
-    
+class Keyword(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    trends = models.ForeignKey(Trend, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+

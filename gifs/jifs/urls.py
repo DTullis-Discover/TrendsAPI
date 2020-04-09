5b19f368647885be0b6c7f1190ae9498953b56d4
sampiecz <samuel.piecz@gmail.com>
2020-04-09 18:45:19 -0500

    Fixed urls.py bug.

--- gifs/jifs/urls.py
+++ gifs/jifs/urls.py
@@ -4,6 +4,6 @@ from gifs.jifs.views import JifDetailView, JifListView
 app_name = "jifs"
 
 urlpatterns = [
-    path("<image:image>/", JifDetailView.as_view(), name="jif-detail"),
+    # path("<image:image>/", JifDetailView.as_view(), name="jif-detail"),
     path("list", JifListView.as_view(), name="jif-list"),
 ]

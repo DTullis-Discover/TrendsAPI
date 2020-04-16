1798e2a1bf174d6ea6fdb52f075595236055af33
sampiecz <samuel.piecz@gmail.com>
2020-04-16 06:04:51 -0500

    Got detail page working. Moved api key to environment variable. Which I think is pointless because this project is public, which is also dumb. So, basically the key is compromised theminute I push this. But it's dev key so it doesn't matter rn. But envs shouldn't be kept in version control unless protected.

--- gifs/jifs/views.py
+++ gifs/jifs/views.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from gifs.jifs.models import Jif 
+import requests
+import json
+import os
 
 class JifDetailView(DetailView):
 
@@ -13,12 +16,21 @@ class JifDetailView(DetailView):
         context['now'] = timezone.now()
         return context
 
-class JifListView(ListView):
+def JifList(request):
 
-    model = Jif 
-    paginate_by = 100  # if pagination is desired
+    url = "https://api.giphy.com/v1/gifs/trending?api_key=" + os.environ['GIPHYKEY'] + "&limit=25&rating=G"
+    payload = {}
+    headers= {}
+    response = requests.request("GET", url, headers=headers, data = payload)
+    data = json.loads(response.text.encode('utf8'))
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['now'] = timezone.now()
-        return context
+    for item in data["data"]:
+        try:
+            jif = Jif.objects.get(url=item["embed_url"])
+        except Jif.DoesNotExist:
+            jif = Jif(url=item["embed_url"], title=item["title"], trending_datetime=item["trending_datetime"])
+            jif.save()
+
+    jifs = Jif.objects.all()
+
+    return render(request, 'jifs/jif_list.html', {"jifs" : jifs})

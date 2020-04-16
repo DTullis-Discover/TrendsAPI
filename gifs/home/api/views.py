284967efbeece9ab15d6e4688a088c3104308d69
sampiecz <samuel.piecz@gmail.com>
2020-04-16 06:43:06 -0500

    Got started on adding api endpoint for jifs sub app. Couldn't get it to display. Nothing is breaking though. Same issue Ben and I had with the trends API. Everything seems fine, nothing is breaking, but if you go to /api/gifs or /api/trends it doesn't display anything. However /api/users works just fine. Not sure what the discrepency is.

--- gifs/home/api/views.py
+++ gifs/home/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from gifs.home.models import Trend, Keyword
-from gifs.home.api.serializer import TrendSerializer 
+from gifs.home.api.serializers import TrendSerializer 
 
 class TrendViewSet(viewsets.ViewSet):
     serializer_class = TrendSerializer

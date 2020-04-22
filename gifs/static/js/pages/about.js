852d966f07ebc20682394406df4630e69d1d921d
sampiecz <samuel.piecz@gmail.com>
2020-04-22 09:26:47 -0500

    Added David's changes since he somehow made his branch a lone dot that cannot be merged. Not sure how to remedy that. Don't have time just pasting files into master.

--- gifs/static/js/pages/about.js
+++ gifs/static/js/pages/about.js
@@ -1,82 +1,136 @@
-import React, { useRef, useEffect, useState } from "react"
-import ReactDOM from "react-dom"
-import { axisLeft, axisRight, curveCardinal, axisBottom, keys, extent, scalePoint, scaleLinear, select, line } from "d3";
+import React, { useState, useRef, useEffect, setState } from 'react'
+import ReactDOM from 'react-dom'
+import { select, line, curveCardinal, axisBottom, scaleLinear, axisRight } from "d3";
 import notData from "./../../json/dummy.json"
 
-function About() {
-	// func vars
-	const svgRef = useRef()
+var incr = 1;
 
-  // Look at json and parse into vars
-  let individualData = JSON.parse(notData[0]["fields"]["data"])
-  let keyword = notData[0]["fields"]["keyword"]
-  let realData = individualData[keyword]
 
-  // Verify vars
-  /*
-  console.log(individualData)
-  console.log(keyword)
-  */
-  console.log(realData)
-
-  // Store test line data here
-  let realDates = []
+function processedData(theData) {
+  let processedData = []
   let x
-  // Loop through date parse epoch
-  for(x in realData){
-    console.log(realData[x])
-    realDates.push(realData[x])
+  let y
+  for( x in theData ) {
+    let singleKey = theData[x]["fields"]["keyword"]
+    let singleData = JSON.parse(theData[x]["fields"]["data"])[singleKey]
+    let singleDate = theData[x]["fields"]["date"]
+    let singleLine = []
+
+    // Loop through date parse epoch
+    for(y in singleData){
+      singleLine.push(singleData[y])
+    }
+
+    let singleObj = {
+      "keyword": singleKey,
+      "date": singleDate,
+      "data": singleData,
+      "line": singleLine
+    }
+    processedData.push(singleObj)
+  }
+  return processedData
+}
+
+function About() {
+
+  const svgRef = useRef();
+
+
+
+  //set data
+  let realDates = processedData(notData)
+  console.log(realDates)
+  let why = new Array()
+
+  var z
+
+  for (z = 0; z < 180; z++)
+  {
+    why.push(realDates[0]["line"][z])
   }
 
-  // Look at the date list
-  console.log(realDates)
+  //console.log(why1);
+  console.log(incr);
 
-  // Set the data
-  const [data, setData] =  useState(realDates);
+  let keys = new Array()
+  var k
+  for(k = 0; k < 220; k++){
+    keys.push(realDates[k]["keyword"])
+  }
 
-  /* 
-	let margin = {top: 30, right: 10, bottom: 10, left: 0},
-		width = 800 - margin.left - margin.right,
-		height = 600 - margin.top - margin.bottom;
-  */
+  console.log(keys);
+
+  function increment(){
+    if(incr<180)
+      setData(realDates[incr++]["line"]);
+  }
+  function decrement(){
+    if(incr>1)
+      setData(realDates[incr--]["line"]);
+  }
+
+  function keyDisplay(){
+    return keys[incr];
+  }
+
+
+  const [data, setData] =  useState(why);
+
 
   //will be called for every data change
   useEffect(() => {
     const svg = select(svgRef.current);
 
-    //x size of svg
+
+    svg
+      .attr("preserveAspectRatio", "xMaxYMid meet")
+      .attr("width", 1200)
+      .attr("height", 558)
+      .attr("viewBox", "0 0 " + 300 + " 151");
+
+      //.attr("viewBox", "0 0 " + 400 + " 20");
+
+   /* const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 50]);
+
+    //y size of svg
+    const yScale = scaleLinear()
+      .domain([0, 125])
+      .range([20, 0]);*/
+
     const xScale = scaleLinear()
       .domain([0, data.length - 1])
       .range([0, 300]);
 
     //y size of svg
     const yScale = scaleLinear()
-      .domain([0, 150])
+      .domain([0, 110])
       .range([150, 0]);
 
-    //x size of svg
-    /*
-    const xAxis = axisBottom(xScale).ticks(data.length).tickFormat(index => index + 1);
-    svg
-      .select(".x-axis")
-      .style("transform", "translateY(150px)")
-      .call(xAxis);
 
-    const yAxis = axisRight(yScale);
-    svg
-      .select(".y-axis")
-      .style("transform", "translateX(300px)")
-      .call(yAxis);
-    */
-    
+
+
+      const yAxis = axisRight(yScale);
+      svg
+        .select(".y-axis")
+        .style("transform", "translateX(300px)")
+        .call(yAxis);
+
+
+    //x size of svg
+
     // generates the "d" attribute of a path element
     const myLine = line()
       .x((value, index) => xScale(index))
       .y(yScale)
       .curve(curveCardinal);
 
-    
-    //render path element, and attaches the "d" attribute from line generator above
+
+
+      //render path element, and attaches the "d" attribute from line generator above
+
     svg
       .selectAll(".line")
       .data([data])
@@ -84,32 +138,46 @@ function About() {
       .attr("class", "line")
       .attr("d", myLine)
       .attr("fill", "none")
-      .attr("stroke", "blue");
+      .attr("stroke", "green");
+
+
+
 
   }, [data]);
 
-	return (
-    <React.Fragment>
-      <svg ref={svgRef}>
-        <g className="x-axis" />
-        <g className="y-axis" />
-      </svg>
-      <br />
-      <br />
-      <br />
-      <button onClick={() => setData(data.map(value => value + 5))}>
-        Update data
-      </button>
-      <button onClick={() => setData(data.filter(value => value  <= 35))}>
-        Filter data
-      </button>
-    </React.Fragment>
-	);
+  const trend = (
+    <div className="container">
+      <div className="row pt-3 pb-2">
+        <div className="col">
+          <h1>{keyDisplay()}</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <svg ref={svgRef}>
+          <g className="x-axis" />
+          <g className="y-axis" />
+          </svg>
+        </div>
+      </div>
+      <div className="row pt-4 pb-2">
+        <div className="col">
+          <button className="btn btn-primary btn-lg" onClick={() => increment()}>See Another Trend</button>
+        </div>
+      </div>
+    </div>
+  );
+
+
+  return (
+    <>
+     {trend}
+    </>
+  );
 
 }
 
-
 ReactDOM.render(
-	<About/>,
-	document.getElementById("react")
-)
+  <About />,
+  document.getElementById('react')
+);

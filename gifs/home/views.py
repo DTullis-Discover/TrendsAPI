d83a8f23919d151158a66b4c07cf26250cdb910e
benteechur <benkluga@gmail.com>
2020-04-22 12:55:51 -0500

    added try/except as well as django message for trends api

--- gifs/home/views.py
+++ gifs/home/views.py
@@ -4,6 +4,7 @@ import pandas as pd
 import json, time, re
 from gifs.home.models import Keyword, Trend
 from django.core import serializers
+from django.contrib import messages
 
 
 # Create your views here.
@@ -18,10 +19,18 @@ def home(request):
     # Generating list of lists of currently trending keywords with punctuation stripped.
     # Punctuation breaks .interest_over_time() method in some cases at least.
     # .interest_over_time() expects a list for parameter kw_list
-    pytrends = TrendReq(hl='en-US', tz=360)
-    response = pytrends.trending_searches(pn='united_states')
-    data = json.loads(response.to_json())["0"]
-    keyword_list = [[pattern1.sub(" ", value)] for key, value in data.items()]
+    try:
+        pytrends = TrendReq(hl='en-US', tz=360)
+        response = pytrends.trending_searches(pn='united_states')
+        data = json.loads(response.to_json())["0"]
+        keyword_list = [[pattern1.sub(" ", value)] for key, value in data.items()]
+    except Exception as e:
+        keyword_list = []
+        print()
+        print("error trending_searches: google is probably rate-limiting you:", e)
+        print()
+        messages.warning(request, "You've already hit the API: trending_searches.")
+
     #print("keyword_list:", keyword_list)
 
     # Get interest over time and store in single dataframe
@@ -37,41 +46,47 @@ def home(request):
     print("Keyword.objects.all():/n", Keyword.objects.all())
     """
 
-    # search for each item separately
-    for term in keyword_list: 
-        pytrends.build_payload(kw_list=term, cat=0, timeframe='now 1-d', geo='US', gprop='')
-        time.sleep(1)
-        # make call
-        data = pytrends.interest_over_time()
-        # drop unused column
-        data = data.drop(labels=['isPartial'], axis='columns')
-        # convert index into str from Datetime
-        new_index = data.index.astype('str')
-        data = data.set_index(new_index)
-        # convert dataframe to dictionary and remove keyword
-        data2 = data.to_dict()[term[0]]
-        #print(data2)
-        
-        if Keyword.objects.filter(name=term[0]).exists():
-            # if keyword already exists, get old object. save only the trend
-            print("object with name '{}' already exists in the db".format(term[0]))
-            # .filter() returns a list, so object will be at [0]
-            keyword = Keyword.objects.filter(name=term[0])[0]
-            #trend = Trend(data=str(data.to_json()), keyword=keyword)
-            trend = Trend(data=json.dumps(data2), keyword=keyword)
-            trend.save()
-        else:
-            # if keyword is new, create a new object for it. save keyword and trend
-            keyword = Keyword(name=term[0])
-            #trend = Trend(data=str(data.to_json()), keyword=keyword)
-            trend = Trend(data=json.dumps(data2), keyword=keyword)
-            keyword.save()
-            trend.save()
-        #print(data)
-        combined_df = pd.concat([combined_df, data], axis=1, sort=False)
+    try:
+        # search for each item separately
+        for term in keyword_list: 
+            pytrends.build_payload(kw_list=term, cat=0, timeframe='now 1-d', geo='US', gprop='')
+            time.sleep(1)
+            # make call
+            data = pytrends.interest_over_time()
+            # drop unused column
+            data = data.drop(labels=['isPartial'], axis='columns')
+            # convert index into str from Datetime
+            new_index = data.index.astype('str')
+            data = data.set_index(new_index)
+            # convert dataframe to dictionary and remove keyword
+            data2 = data.to_dict()[term[0]]
+            #print(data2)
+            
+            if Keyword.objects.filter(name=term[0]).exists():
+                # if keyword already exists, get old object. save only the trend
+                print("object with name '{}' already exists in the db".format(term[0]))
+                # .filter() returns a list, so object will be at [0]
+                keyword = Keyword.objects.filter(name=term[0])[0]
+                #trend = Trend(data=str(data.to_json()), keyword=keyword)
+                trend = Trend(data=json.dumps(data2), keyword=keyword)
+                trend.save()
+            else:
+                # if keyword is new, create a new object for it. save keyword and trend
+                keyword = Keyword(name=term[0])
+                #trend = Trend(data=str(data.to_json()), keyword=keyword)
+                trend = Trend(data=json.dumps(data2), keyword=keyword)
+                keyword.save()
+                trend.save()
+            #print(data)
+            combined_df = pd.concat([combined_df, data], axis=1, sort=False)
+    except Exception as e:
+        print()
+        print("error interest_over_time: google is probably rate-limiting you:", e)
+        print()
+        messages.warning(request, "You've already hit the API: interest_over_time.")
         
     # see django output in terminal for verification
-    print(combined_df)
+    print("combined_df: \n", combined_df)
     print("Keyword.objects.all():", Keyword.objects.all())
     print("Keyword.objects.count():", Keyword.objects.count())
     print("Trend.objects.all():", Trend.objects.all())
